10677efd29fb2411ffab1b40f9fc644905fe3969
Andrew Ewert <andrew@vestmed.com>
2022-11-29 20:42:40 -0600

    Attempt something

--- server/server.js
+++ server/server.js
@@ -12,6 +12,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static(path.resolve(__dirname, './build')));
+
 mongoose.connect(connString, {
     useNewUrlParser: true,
     //useFindAndModify: false,
